3487d1a9ac0d4c42e1a484c5540bd4b064a548b8
Donik Goxha <donik.godzha@students.finki.ukim.mk>
2025-01-04 14:08:58 +0100

    adding new or just working on existing... idk anymore

--- main.cpp
+++ main.cpp
@@ -1,67 +1,6 @@
 #include <iostream>
 using namespace std;
 
-class Example {
-private:
-    int a;
-    int b;
-
-public:
-    // //default constructor
-    // Example() {
-    //     cout << "Default Constructor is called" << endl;
-    //     a = 0;
-    //     b = 0;
-    // }
-
-    //constructor with args
-    Example(int aa = 0, int bb = 0) {
-        cout << "Constructor with args is called" << endl;
-        a = aa;
-        b = bb;
-    }
-
-    ~Example() {
-        cout << "Destructor is called" << endl;
-    }
-
-    //get methods
-    int getA() {
-        return a;
-    }
-
-    int getB() {
-        return b;
-    }
-
-    //set methods
-    void setA(int aa) {
-        a = aa;
-    }
-
-    void setB(int bb) {
-        b = bb;
-    }
-
-    void print() {
-        cout << "Example: " << a << " " << b << endl;
-    }
-};
-
 int main() {
-    Example e;
-    e.print();
-    //e.a; //a is a private method
-    Example e1(10, 19);
-    e1.print();
-    Example e2;
-    Example e3(100, 100);
-    // e.print();
-    // e1.print();
-    e2.setA(100);
-    e2.setB(-10);
-    e2.print();
-    e2.print();
-    e3.print();
     return 0;
 }
